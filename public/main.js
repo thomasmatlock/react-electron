3bf21d1754259f63c19940fd9f1f4fe254bb8b08
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-07-23 23:56:04 -0400

    testing

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ require('@electron/remote/main').initialize()
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
+    width: 1600,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
